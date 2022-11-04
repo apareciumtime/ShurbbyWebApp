678f0f13f0e945bd23ae5a641c8e276df39ac1a6
apareciumtime <78253494+apareciumtime@users.noreply.github.com>
2022-11-04 11:27:42 +0700

    Update shrubbypage

---
+++ ShurbbyWebApp/resources/views/components/tag-shrubby.blade.php
@@ -0,0 +1,27 @@
+<style>
+.tag-shrubby-frame{
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+
+    position:relative;
+
+    width: fit-content;
+    height: 43px;
+
+    padding:0px 8px;
+
+    background: #646F6D;
+    border-radius: 8px;
+}
+
+.text-frame{
+    white-space: nowrap;
+}
+</style>
+
+<div class="tag-shrubby-frame">
+    <div class="text-frame">
+        {{$name}}
+    </div>
+</div>
