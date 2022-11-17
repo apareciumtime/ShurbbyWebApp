73f7c1c6d4aa4588df0efcd96f4601cf67f026bc
apareciumtime <78253494+apareciumtime@users.noreply.github.com>
2022-11-17 10:57:42 +0700

    Edit class of verify-form

--- ShurbbyWebApp/resources/views/components/comment-shrubby.blade.php
+++ ShurbbyWebApp/resources/views/components/comment-shrubby.blade.php
@@ -305,12 +305,12 @@
             </label>
             <input id="decreasecredit" type="submit" style="display: none;">
         </form>
-        <form id="credit-form" action="{{route('accept.comment',['id'=>$comment->id])}}" method="POST" >
+        <form id="verify-form" action="{{route('accept.comment',['id'=>$comment->id])}}" method="POST" >
             @csrf
         </form>
         
         @if($comment->accept==true)
-            <button class="comment-shrubby-verified-btn" type="submit" form="credit-form">
+            <button class="comment-shrubby-verified-btn" type="submit" form="verify-form">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="comment-shrubby-verified-icon">
                     <path d="M470.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L192 338.7 425.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"/>
                 </svg>
@@ -320,7 +320,7 @@
             </div>
             
         @else
-            <button class="comment-shrubby-verified-btn" type="submit" form="credit-form">
+            <button class="comment-shrubby-verified-btn" type="submit" form="verify-form">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="comment-shrubby-unverify-icon">
                     <path d="M470.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L192 338.7 425.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"/>
                 </svg>
